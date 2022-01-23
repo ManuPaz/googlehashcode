60cc48381aff6bf9e1a7841a57e519e09efc0fcb
ManuPaz <manu.paz.pintor@gmail.com>
2022-01-23 14:03:49 +0100

    dasdasd

--- qualification_round_2021.in/main.py
+++ qualification_round_2021.in/main.py
@@ -13,29 +13,28 @@ Created on Sat Jan 22 15:01:38 2022
 import entradaDatos
 import pandas as pd
 import numpy as np
-import computarpuntos 
-import escribirEnArchivo 
+import computarpuntos
+import escribirEnArchivo
 import time
 import algoritmos
-  
-nombreArchivo="f.txt" 
+
+nombreArchivo="f.txt"
 PRINT=False
-print("hola")         
-   
+        
+
 cabecera,numerocalles,numerocoches,numerointersecciones,adyacencia,incidencia,adyacencia1,incidencia1,interseccionesvscohes,cochesvsinteresecciones,rotondas,calles,dicCalles,arrayCalles,cars=entradaDatos.entradaDatos(nombreArchivo)
-#%%algotimo           
+#%%algotimo
 parametros={}
 parametros["ciclo"]=15
 algoritmos.algoritmo1(cabecera,numerocalles,numerocoches,numerointersecciones,adyacencia,incidencia,adyacencia1,incidencia1,interseccionesvscohes,cochesvsinteresecciones,rotondas,calles,dicCalles,arrayCalles,cars,**parametros)
-       
 
-    
-    
-    
+
+
+
+
 
 #%%
 escribirEnArchivo.escribirEnArchivo(incidencia,arrayCalles,numerointersecciones)
 sumaPuntos=0
 sumaPuntos=computarpuntos.computarpuntos("solucion.txt",nombreArchivo,sumaPuntos,cochesvsinteresecciones,calles,dicCalles,interseccionesvscohes,PRINT)
 print(sumaPuntos)
-    
