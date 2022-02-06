270f8b8fc028e2d773c71cfcdac9f033bd373ba1
ManuPaz <manu.paz.pintor@gmail.com>
2022-02-06 02:27:32 +0100

    2019 qualification

--- 2019qualification/escribirArchivo.py
+++ 2019qualification/escribirArchivo.py
@@ -5,17 +5,14 @@ def escribirSolucion(solucion,archivo):
     escribirEnArchivo(solucionAEscribir,archivo)
 def escribirEnArchivo(solucionAEscribir,archivo):
     file=open(archivo,"w")
+ 
     if solucionAEscribir is not None and len( solucionAEscribir)>0:
+        file.writelines(str(len(solucionAEscribir))+"\n")
         for linea in solucionAEscribir:
-            if isinstance(linea, int) or isinstance(linea, float) or isinstance(linea, str):
-                file.writelines(str(linea)+"\n")
-            #print(linea)
-            else:
-                cadena=""
-                for elem in linea:
-                    cadena+=(str(elem)+" ")
-                cadena=cadena[:-1]
-                file.writelines(cadena+"\n")
+                for e in linea:
+                    file.writelines(str(e)+" ")
+                file.writelines("\n")
+          
     else:
         print("Solucion vacia")
     file.close()
--- 2019qualification/main.py
+++ 2019qualification/main.py
@@ -5,21 +5,11 @@ import funciones
 import clases
 import numpy as np
 import pandas as pd
-#%%
-archivos=["a_example.txt","b_lovely_landscapes.txt","c_memorable_moments.txt","d_pet_pictures.txt","e_shiny_selfies.txt"]
-archivo=archivos[1]
-infogeneral,diccionarioFotos,diccionarioTags=leerArchivo.leerArchivo("entrada/"+archivo)
-numFotos=infogeneral[0]
-numFotosVerticales=len([e for e,i in diccionarioFotos.items() if i["vertical"]==True ])
-numFotosHorizonales=len([e for e,i in diccionarioFotos.items() if i["vertical"]==False])
-print("Fotos totales %s,verticales %s, horizonateles %s"%(numFotos,numFotosVerticales,numFotosHorizonales))
+import trace
+import sys
 
 #%%
-solucion=algoritmo.algoritmo1(infogeneral,diccionarioFotos,diccionarioTags)
-#a=funciones.devolverColumnaDiccionario(diccionario1, "a")
-#matriz=funciones.crearmatriz((2,2),10)
-
-#%%
+#resumen de los archivos para saber que algoritmo aplicar
 archivos=["a_example.txt","b_lovely_landscapes.txt","c_memorable_moments.txt","d_pet_pictures.txt","e_shiny_selfies.txt"]
 for archivo in archivos:
     print(archivo)
@@ -29,30 +19,76 @@ for archivo in archivos:
     numFotosHorizonales=len([e for e,i in diccionarioFotos.items() if i["vertical"]==False])
     numTags=len(diccionarioTags)
     fotosPorTag=np.mean([len(e) for e in diccionarioTags.values()] )
+    tagsPorFotoDesv=np.std([e["numTags"]  for e in diccionarioFotos.values()] )
     tagsPorFoto=np.mean([e["numTags"] for e in diccionarioFotos.values()])
-    solucion=algoritmo.algoritmo1(infogeneral,diccionarioFotos,diccionarioTags)
+    #solucion=algoritmo.algoritmo1(infogeneral,diccionarioFotos,diccionarioTags,1)
     print("Fotos totales %s,verticales %s, horizonateles %s, tags totales %s"%(numFotos,numFotosVerticales,numFotosHorizonales,numTags))
-    print("Media de fotos por tag %s"% fotosPorTag)
-    print("Media de tags por  foto %s"% tagsPorFoto)
+    print("Media de fotos por tag %s"%( fotosPorTag  ))
+    print("Media de tags por  foto %s,desv tags por foto %s"%( tagsPorFoto,tagsPorFotoDesv))
     print("---------------\n")
-#en los que hay fotos verticales hay que ver como se juntan primero para tener el mayor numero de fotos distintas y mas relaciones con el resto
-#en e;muchas verticales y muchas fotos por tag, hay que juntar bien las fotos teniendo en cuenta con que otras fotos se van a juntar despues
-#en d igual a e,pero hay horizontales
-#en c hay horizontales pero menos tags por foto: al agrupar solo hay que conseguir muchos tags en cada slice
-#en b no hay verticales, solo hay que preocuparse de comos se juntan
-
-
-#prueba de hierarchical clustering
 #%%
+archivo=archivos[3]
+infogeneral,diccionarioFotos,diccionarioTags=leerArchivo.leerArchivo("entrada/"+archivo)
+diccionarioFotos1=diccionarioFotos.copy()
+#solucion=algoritmo.algoritmo1(infogeneral,diccionarioFotos,diccionarioTags,1)
+solucion=algoritmo.algoritmo2(infogeneral,diccionarioFotos,diccionarioTags,1,paramMinimoH=4,paramMaximoH=7,paramMinimoV=3,paramMaximoV=6,paramParada=0.05)
 
-
-
-#%%
-solucion=algoritmo.algoritmo1(infogeneral,diccionarioFotos,diccionarioTags)
-#a=funciones.devolverColumnaDiccionario(diccionario1, "a")
-#matriz=funciones.crearmatriz((2,2),10)
-
-#%%
+print(archivo)
 archivoSolucion="salida/solucion_"+archivo[0]
 escribirArchivo.escribirSolucion(solucion, archivoSolucion)
+#%%
+#print(len(solucion))
+puntos=0
+for i,l in enumerate(solucion[:-1]):
+    tags1=set([tag for e in l for tag in diccionarioFotos1[e]["tags"]])
+    tags2=set([tag for e in solucion[i+1] for tag in diccionarioFotos1[e]["tags"]])
+    inter=tags1.intersection(tags2)
+    puntos+=min(len(inter),len(tags2)-len(inter),len(tags1)-len(inter))
+print(puntos)
+#b 208803
+#c 1716   
+#e 276000 
+#d 185000
+    
+
+#%%
+infogeneral,diccionarioFotos,diccionarioTags=leerArchivo.leerArchivo("entrada/"+archivos[3])
+#%%
+#herramienta que da el tiempo total y llamadas a cada funcion
+import cProfile
+
+#perf=cProfile.run("algoritmo.algoritmo2(infogeneral,diccionarioFotos,diccionarioTags,1)")
+perf=cProfile.run("algoritmo.algoritmo2(infogeneral,diccionarioFotos,diccionarioTags,1,paramMinimoH=2,paramMaximoH=8,paramMinimoV=2,paramMaximoV=6,paramParada=0.2)")
+
+
+
+#%%
+#ejecucion de todos los archivos en multiprocessing
+import time
+import multiprocessing  as mp
+
+def ejecutar(i,archivo, shared_list):
+    tiempo1=time.time()
+     
+    infogeneral,diccionarioFotos,diccionarioTags=leerArchivo.leerArchivo("entrada/"+archivo)
+    diccionarioFotos1=diccionarioFotos.copy()
+    #solucion=algoritmo.algoritmo1(infogeneral,diccionarioFotos,diccionarioTags,1)
+    solucion=algoritmo.algoritmo2(infogeneral,diccionarioFotos,diccionarioTags,1,paramMinimoH=2,paramMaximoH=8,paramMinimoV=1,paramMaximoV=7,paramParada=0.01)
+    
+    print(archivo)
+    archivoSolucion="salida/solucion_"+archivo[0]
+    escribirArchivo.escribirSolucion(solucion, archivoSolucion)
+    tiempo2=time.time()
+    print("Tiempo total %s,archivo %s"%((tiempo2-tiempo1),archivo))
+sharedList=mp.Manager().list() 
+process=[]
+for i,archivo in enumerate(archivos[3:5]):
+    sharedList.append(0)
+
+    
+    process.append(mp.Process(target=ejecutar,args=[i,archivo,sharedList]))
+    process[i].start()
+    for proces in process:
+        proces.join()
+        
 
