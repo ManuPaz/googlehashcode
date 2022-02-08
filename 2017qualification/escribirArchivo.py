9ab7e113f267f8246942fee1ea5a0db01d2f8011
ManuPaz <manu.paz.pintor@gmail.com>
2022-02-08 19:43:03 +0100

    Escribir archivo ok

--- 2017qualification/escribirArchivo.py
+++ 2017qualification/escribirArchivo.py
@@ -1,17 +1,14 @@
 import clases
 
-def escribirSolucion(solucion,archivo):
-    solucionAEscribir=solucion
-    escribirEnArchivo(solucionAEscribir,archivo)
-def escribirEnArchivo(solucionAEscribir,archivo):
+def escribirEnArchivo(a, b, archivo):
     file=open(archivo,"w")
     if solucionAEscribir is not None and len( solucionAEscribir)>0:
-        file.writelines(numCellsBackbone+"\n")
-        for cellB in numCellsBackbone:
-            file.writelines( + " " + +"\n")
-        file.writelines(numCellsRouters+"\n")
-        for cellR in numCellsRouters:
-            file.writelines( + " " + +"\n")
+        file.writelines(len(a)+"\n")
+        for cellB in a:
+            file.writelines( cellB[0]+ " " + cellB[1]+"\n")
+        file.writelines(len(b)+"\n")
+        for cellR in b:
+            file.writelines( cellR[0]+ " " + cellR[1]+"\n")
     else:
         print("Solucion vacia")
     file.close()
